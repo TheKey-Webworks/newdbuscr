f527e405a6c0bb41b7fe749598bd3880fa7a2e0d
TheKey-Webworks <163067791+TheKey-Webworks@users.noreply.github.com>
2024-09-18 06:32:22 -0300

    Update dbu_new.lua

--- dbu_new.lua
+++ dbu_new.lua
@@ -292,7 +292,9 @@ wait(7)
 local textoStats = script.Parent.faseName
 -- Funciones
 local function mundoDeBills()
+		if checkValue < 250000000 then return
 	game:GetService("ReplicatedStorage").Package.Events.TP:InvokeServer("Vills Planet")
+		end
 end
 local function statsAltos()
 	textoStats.Text = "Subiendo manualmente"
